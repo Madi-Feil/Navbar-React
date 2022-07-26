6e884fda09e5f8b14196537d412ceca306e9e892
Madi Greeley <98246671+Madi-Feil@users.noreply.github.com>
2022-07-26 13:38:18 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Context from "./context/Context";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import FormWrapper from "./components/FormWrapper";
+
+function App() {
+  const [name, setName] = useState("");
+  return (
+  <div>
+    <Context.Provider value={{ name, setName }}>
+      <Navbar />
+      <FormWrapper />
+    </Context.Provider>
+  </div>
+  )
+}
+
+export default App;
